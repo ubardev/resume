0c07c5fab88295c7d5676ad4c974ef6da2362f8f
Ubar <bellin83@gmail.com>
2022-06-10 12:32:06 +0900

    :heavy_plus_sign: Add: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "media",
+  plugins: [require("@tailwindcss/forms")],
+};
